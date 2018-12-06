4f9be3a7353f16497c193c7334d66081c1178461
Miguel Bolivar <MBolivar360@gmail.com>
2018-12-06 12:38:23 -0400

    Rendering news title with helper method.-

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../blocs/comments_provider.dart';
+import '../blocs/comments_bloc.dart';
+import '../models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -7,13 +10,53 @@ class NewsDetail extends StatelessWidget {
 
   @override
     Widget build(BuildContext context) {
+      final CommentsBloc bloc = CommentsProvider.of(context);
       return Scaffold(
         appBar: AppBar(
           title: Text('Detail'),
         ),
-        body: Center(
-          child: Text('Im a details page for id $itemId'),
-        ),
+        body: buildBody(bloc),
       );
     }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (BuildContext context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          final itemFuture = snapshot.data[itemId];
+          return FutureBuilder(
+            future: itemFuture,
+            builder: (BuildContext context, AsyncSnapshot<ItemModel> itemSnapshot) {
+              if (!itemSnapshot.hasData) {
+                return Text('Loading...');
+              } else {
+                return buildTitle(itemSnapshot.data);
+              }
+            },
+          );
+        }
+      },
+    );
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+      alignment: Alignment.topCenter,
+      margin: EdgeInsets.all(10.0),
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
 }
